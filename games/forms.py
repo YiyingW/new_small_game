5d17013b0dc5a23fab85e35609c1fc14e38d6819
Yiying Wang <wangyydut@gmail.com>
2018-03-13 20:11:29 -0700

    add test cases

--- games/forms.py
+++ games/forms.py
@@ -3,7 +3,12 @@ from .models import History
 
 class GameForm(forms.ModelForm):
     player_name = forms.CharField(min_length=1, max_length=200)
-    comment = forms.CharField(min_length=1, max_length=2000)
+    comment = forms.CharField(
+                            widget=forms.Textarea(
+                                attrs={'rows':5, 'placeholder': 'What is on your mind?'}
+                            ),
+                            min_length=1, 
+                            max_length=4000)  # TODO: this placeholder is not working.
 
     class Meta:
         model = History
--- games/tests.py
+++ games/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.urls import reverse
-from .views import index, detail
+from .views import index, detail, summary
+from .forms import GameForm
+from .models import History
 
 # Create your tests here.
 
@@ -31,14 +33,43 @@ class DetailTests(TestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
-    def test_detail_url_resolves_game0_view(self):
+    def test_detail_url_resolves_game_view(self):
         view = resolve('/0/')
         self.assertEquals(view.func, detail)
 
-    def test_detail_url_resolves_game1_view(self):
-        view = resolve('/1/')
-        self.assertEquals(view.func, detail)
+    def test_detail_view_contains_link_to_index_page(self):
+        game_url = reverse('detail', args=['0'])
+        response = self.client.get(game_url)
+        index_url = reverse('games:index')
+        self.assertContains(response, 'href="{0}"'.format(index_url))
 
-    def test_detail_url_resolves_game2_view(self):
-        view = resolve('/2/')
-        self.assertEquals(view.func, detail)
+class SummaryTests(TestCase):
+
+    def test_summary_view_status_code(self):
+        url = reverse('summary')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_summary_url_resolves_summary_view(self):
+        view = resolve('/summary/')
+        self.assertEquals(view.func, summary)
+
+    def test_summary_view_contains_link_to_index_page(self):
+        summary_url = reverse('summary')
+        response = self.client.get(summary_url)
+        index_url = reverse('games:index')
+        self.assertContains(response, 'href="{0}"'.format(index_url))
+
+class GameFormTests(TestCase):
+
+    def test_new_user_comment_valid_post_data(self):
+        url = reverse('detail', kwargs={'game_id': '0'})
+        data = {
+            'player_name' : "Xiao Mao",
+            'comment': "Fun game"
+        }
+        response =  self.client.post(url, data)
+        self.assertTrue(History.objects.exists())
+
+
+
